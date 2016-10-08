1fd20222cbc6fe15beaecc2f79afa466ad8b8d47
Alex Saber <Alex Saber>
2016-10-08 11:41:17 -0500

    set up grunt

--- javascripts/es6.js
+++ javascripts/es6.js
@@ -1,104 +1,104 @@
-//const
-const a = 8;
-console.log('a',a);
-// a = 9;
-// console.log('a2',a);
+// //const
+// const a = 8;
+// console.log('a',a);
+// // a = 9;
+// // console.log('a2',a);
 
-// const outputDiv = $('#output');
-
-
-//let
-let b = 8;
-console.log('b',b);
-b = 12;
-console.log('b2',b);
-
-function varTest() {
-  var x = 1;
-  if(true) {
-    var x = 2;
-    console.log('inside if: ',x);
-  }
-  console.log('outside if: ',x);
-}
-varTest();
-
-function letTest() {
-  let x = 1;
-  if(true) {
-    let x = 2;
-    console.log('inside if: ',x);
-  }
-  console.log('outside if: ',x);
-}
-letTest();
+// // const outputDiv = $('#output');
 
 
-//arrow functions
-var sum = function() {
-  let a = 1, b = 2;
-  return a + b;
-}
-console.log('sum: ',sum());
+// //let
+// let b = 8;
+// console.log('b',b);
+// b = 12;
+// console.log('b2',b);
 
-var sum2 = () => {
-  let a = 1, b = 2;
-  return a + b;
-}
-console.log('sum2: ',sum2());
+// function varTest() {
+//   var x = 1;
+//   if(true) {
+//     var x = 2;
+//     console.log('inside if: ',x);
+//   }
+//   console.log('outside if: ',x);
+// }
+// varTest();
 
-var reflect = function(value) {
-  return value;
-}
-console.log("reflect: ",reflect("hi"));
-
-var reflect2 = (value) => value;
-console.log("reflect2: ",reflect2("hi"));
-
-var realSum = function(num1,num2) {
-  return num1 + num2;
-}
-console.log('realSum: ',realSum(2,3));
-
-var realSum2 = (num1,num2) => {
-  return num1 + num2;
-}
-console.log('realSum2: ',realSum2(5,4));
+// function letTest() {
+//   let x = 1;
+//   if(true) {
+//     let x = 2;
+//     console.log('inside if: ',x);
+//   }
+//   console.log('outside if: ',x);
+// }
+// letTest();
 
 
-//string templates
-let m = "bite me";
-console.log('m: ',m);
-let r = `hello my name is ${m}`;
-console.log("r: ",r);
+// //arrow functions
+// var sum = function() {
+//   let a = 1, b = 2;
+//   return a + b;
+// }
+// console.log('sum: ',sum());
+
+// var sum2 = () => {
+//   let a = 1, b = 2;
+//   return a + b;
+// }
+// console.log('sum2: ',sum2());
+
+// var reflect = function(value) {
+//   return value;
+// }
+// console.log("reflect: ",reflect("hi"));
+
+// var reflect2 = (value) => value;
+// console.log("reflect2: ",reflect2("hi"));
+
+// var realSum = function(num1,num2) {
+//   return num1 + num2;
+// }
+// console.log('realSum: ',realSum(2,3));
+
+// var realSum2 = (num1,num2) => {
+//   return num1 + num2;
+// }
+// console.log('realSum2: ',realSum2(5,4));
 
 
-//object literal property shorthand
-let wow = "Hi there", es6 = "ES6", numNum = () => {console.log("howdy");}
-
-let myOldObject = {
-  wow: wow,
-  es6: es6,
-  numNum: numNum
-}
-
-console.log('myOldObject: ',myOldObject);
-
-let myNewObject = {
-  wow,es6,numNum
-}
-console.log('myNewObject: ',myNewObject);
+// //string templates
+// let m = "bite me";
+// console.log('m: ',m);
+// let r = `hello my name is ${m}`;
+// console.log("r: ",r);
 
 
-//method properties
-var e5Obj = {
-  foo: function() {console.log('foo')},
-  bar: function() {console.log('bar')}
-}
-console.log('e5Obj: ',e5Obj);
+// //object literal property shorthand
+// let wow = "Hi there", es6 = "ES6", numNum = () => {console.log("howdy");}
 
-var es6Obj = {
-  foo(){console.log("foo")},
-  bar(){console.log("bar")}
-}
-console.log('es6Obj: ',es6Obj);
+// let myOldObject = {
+//   wow: wow,
+//   es6: es6,
+//   numNum: numNum
+// }
+
+// console.log('myOldObject: ',myOldObject);
+
+// let myNewObject = {
+//   wow,es6,numNum
+// }
+// console.log('myNewObject: ',myNewObject);
+
+
+// //method properties
+// var e5Obj = {
+//   foo: function() {console.log('foo')},
+//   bar: function() {console.log('bar')}
+// }
+// console.log('e5Obj: ',e5Obj);
+
+// var es6Obj = {
+//   foo(){console.log("foo")},
+//   bar(){console.log("bar")}
+// }
+// console.log('es6Obj: ',es6Obj);
